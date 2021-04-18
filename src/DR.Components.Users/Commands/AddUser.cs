46dec38b8dffa52553d68c7b99f971286afd026b
Krur Schak <krurschak@gmail.com>
2021-04-18 10:22:09 +0200

    Initial code commit

---
+++ src/DR.Components.Users/Commands/AddUser.cs
@@ -0,0 +1,12 @@
+﻿using DR.Packages.MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DR.Components.Users.Commands
+{
+    public interface AddUser : ICommand
+    {
+        Guid Id { get; }
+    }
+}
