019250f7955284a70c8fa29e53fc5d7cdbe1693c
Krur Schak <krurschak@gmail.com>
2021-04-18 18:33:03 +0200

    add create user command and user created event

---
+++ src/DR.Components.Users/Events/UserCreated.cs
@@ -0,0 +1,19 @@
+﻿using DR.Packages.MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DR.Components.Users.Events
+{
+    public interface UserCreated : IEvent
+    {
+        public Guid Id { get; set; }
+        public string IdentitySub { get; set; }
+        public string IdentityIss { get; set; }
+        public string Email { get; set; }
+        public int? Salutation { get; set; }
+        public string Title { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+    }
+}
