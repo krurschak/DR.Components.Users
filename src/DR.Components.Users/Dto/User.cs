cf903c2464e7ea3d850bf1cdbad31964d7e66e42
Krur Schak <krurschak@gmail.com>
2021-05-08 13:18:56 +0200

    add user dto

---
+++ src/DR.Components.Users/Dto/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DR.Components.Users.Dto
+{
+    [Serializable]
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public int Salutation { get; set; }
+        public string Title { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+    }
+}
